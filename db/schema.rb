23bd5c5cab1c85d14fe4eea8f2e033591c4daa94
Admir Draganovic <admir.draganovic@gmail.com>
2016-03-02 12:43:15 -0700

    add db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506203157) do
+ActiveRecord::Schema.define(version: 20160302192635) do
+
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.integer  "teacher_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "age"
+    t.integer  "current_score", default: 0
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string   "first_name"
